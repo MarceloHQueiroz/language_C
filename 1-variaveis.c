749e668790b6e42118921477d28ae1ce9437b8cf
Marcelo Queiroz <queiroz.engsoftware@gmail.com>
2026-06-09 00:07:24 -0300

    Primeiro codigo de variaveis em C

---
+++ 1-variaveis.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+
+int main()
+{
+    char nome[] = "Marcelo";
+    int idade = 25;
+    float altura = 1.75;
+
+    printf("Nome: %s\n", nome);
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    
+    return 0;
+
+}
